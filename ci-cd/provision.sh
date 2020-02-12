03efef586e226a7e32984ba84ad89e4632541de1
Dmitriy Kononov <kononov.dmitriy@gmail.com>
2020-02-12 18:34:57 +0600

    update

--- ci-cd/provision.sh
+++ ci-cd/provision.sh
@@ -10,8 +10,6 @@ kubectl create configmap db-gitea --from-file=./files/gitea.sql
 helm install --name postgresql --set postgresqlUsername=gitea,postgresqlPassword=gitea1234,postgresqlDatabase=gitea,initdbScriptsConfigMap=db-gitea stable/postgresql
 while [[ $(kubectl get pods postgresql-postgresql-0 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pg" && sleep 2; done
 
-helm install --name gitea charts/gitea
-
 # jenkins
 kubectl create secret generic j-cred --from-file=./files/credentials.xml
 helm install --name jenkins stable/jenkins -f charts/helm-jenkins-values.yaml
@@ -24,7 +22,11 @@ echo "Jenkins has been initialized, waiting for readiness"
 while [[ $(kubectl get pods -l app.kubernetes.io/name=jenkins -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for jenkins" && sleep 2; done
 echo "Jenkins is ready to serve"
 
-while [ -z $external_ip ]; do echo "Waiting for end point..."; external_ip=$(kubectl get svc nginx-nginx-ingress-controller --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}"); [ -z "$external_ip" ] && sleep 2; done; echo "End point ready $external_ip"
+while [ -z $external_ip ]; do echo "Waiting for end point..."; external_ip=$(kubectl get svc nginx-ingress-controller --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}"); [ -z "$external_ip" ] && sleep 2; done; echo "End point ready $external_ip"
+
+# gitea
+sed -i "s@# externalHost: git.workshop.msb.com@externalHost: $external_ip@" ./charts/gitea/values.yaml
+helm install --name gitea charts/gitea
 
 # service account for the jenkins agent
 kubectl create serviceaccount build-robot && kubectl create clusterrolebinding build-robot --clusterrole=cluster-admin --serviceaccount=default:build-robot
